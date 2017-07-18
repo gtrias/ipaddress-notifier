78cfb019fc358b7e3eefa14efecb29f422b49739
Genar Trias Ortiz <genar@acs.li>
2017-07-18 21:53:24 +0200

    First IP Address notification

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const getIP = require('external-ip')();
+var config        = require('config');
+var TelegramBot   = require('node-telegram-bot-api');
+var tgtoken       = config.get('telegram.token');
+var tgNotifyUsers = config.get('telegram.notifyUsers');
+const getIP       = require('external-ip')();
+
+var tgBot = new TelegramBot(tgtoken, { polling: true });
 
 getIP((err, ip) => {
-    if (err) {
-        // every service in the list has failed
-        throw err;
-    }
-    console.log(ip);
+  if (err) {
+    // every service in the list has failed
+    throw err;
+  }
+  notify(ip);
 });
+
+// send notification to tg
+function notify(msg) {
+  // Telegram sending
+  for (var i = 0; i < tgNotifyUsers.length; i++) {
+    tgBot.sendMessage(tgNotifyUsers[i], msg);
+  }
+}
